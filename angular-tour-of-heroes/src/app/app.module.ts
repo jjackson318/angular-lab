778eb743f12e4afa9e651c49255fe5244b3bb5e6
Joel Jackson <74617125+jjackson318@users.noreply.github.com>
2020-12-14 20:39:23 -0600

    commit six
    begining of step six

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -9,7 +9,11 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
-
+@NgModule({
+  imports: [
+    HttpClientModule,
+  ],
+})
 @NgModule({
   imports: [
     BrowserModule,
